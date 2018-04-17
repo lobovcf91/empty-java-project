9c3e8a5b2c776a590d2f9ace0f50d74447f228b9
lobo <lobo.soldado@gmail.com>
2018-04-17 14:10:47 +0200

    mi otro commit

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,24 +6,13 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-       byte b= 65;  // 8 bits
-       char c= 'A'; //16 bits
-       short s= 65; // 16 bits
-       int i = 65;  //32 bits
-       long l= 65L; //64 bits
-       float f= 65f;    //32 bits
-       double d= 65.55; //64 bits
-       boolean bol= true;   //1 bit
 
-       System.out.println(b);
-       System.out.println(c);
-       System.out.println(s);
-       System.out.println(i);
-       System.out.println(l);
-       System.out.println(f);
-       System.out.println(d);
-       System.out.println(bol);
-       
+       System.out.println(2+5-3*2);
+       System.out.println(2*4*2/2);
+       System.out.println(1/2);
+       System.out.println(5*(1/2));
+       System.out.println(5f*1/2);
+
     }
 
 }
