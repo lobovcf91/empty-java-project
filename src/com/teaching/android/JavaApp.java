7c8fd1df697bd46057ca6ef028bd338a74166b4c
lobo <lobo.soldado@gmail.com>
2018-04-17 13:04:15 +0200

    mi primer commit

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -6,7 +6,24 @@ package com.teaching.android;
 public class JavaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+       byte b= 65;  // 8 bits
+       char c= 'A'; //16 bits
+       short s= 65; // 16 bits
+       int i = 65;  //32 bits
+       long l= 65L; //64 bits
+       float f= 65f;    //32 bits
+       double d= 65.55; //64 bits
+       boolean bol= true;   //1 bit
+
+       System.out.println(b);
+       System.out.println(c);
+       System.out.println(s);
+       System.out.println(i);
+       System.out.println(l);
+       System.out.println(f);
+       System.out.println(d);
+       System.out.println(bol);
+       
     }
 
 }
