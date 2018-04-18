e0c9f588af60709bc4be55cd737a7d7ceec69282
lobo <lobo.soldado@gmail.com>
2018-04-18 09:09:10 +0200

    otro commint das

--- src/com/teaching/android/JavaApp.java
+++ src/com/teaching/android/JavaApp.java
@@ -1,5 +1,7 @@
 package com.teaching.android;
 
+import java.util.Scanner;
+
 /**
  * Empty Java App for teaching purposes.
  */
@@ -7,12 +9,21 @@ public class JavaApp {
 
     public static void main(String[] args) {
 
-       System.out.println(2+5-3*2);
-       System.out.println(2*4*2/2);
-       System.out.println(1/2);
-       System.out.println(5*(1/2));
-       System.out.println(5f*1/2);
+        System.out.println(2 + 5 - 3 * 2);
+        System.out.println(2 * 4 * 2 / 2);
+        System.out.println(1 / 2);
+        System.out.println(5 * (1 / 2));
+        System.out.println(5f * 1 / 2);
 
+        Scanner keyboard = new Scanner(System.in);
+        int valor = keyboard.nextInt();
+        int a = valor % 2;
+        if (a == 0) {
+            System.out.println("par");
+        } else {
+
+            System.out.println("impar");
+
+        }
     }
-
 }
